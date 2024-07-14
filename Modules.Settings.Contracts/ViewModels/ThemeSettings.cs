3abbae20daf6c9861c5e92d27b100c39bea4c36d
zoltanvi <viczezoltan@gmail.com>
2024-07-14 13:18:25 +0200

    Change default app border color

--- Modules.Settings.Contracts/ViewModels/ThemeSettings.cs
+++ Modules.Settings.Contracts/ViewModels/ThemeSettings.cs
@@ -7,6 +7,6 @@ public class ThemeSettings : SettingsBase
     public bool DarkMode { get; set; } = true;
     public MaterialThemeStyle ThemeStyle { get; set; } = MaterialThemeStyle.TonalSpot;
     public string SeedColor { get; set; } = "#1D64DD";
-    public string AppBorderColor { get; set; } = "#BDBDBD";
+    public string AppBorderColor { get; set; } = "#5C6BC0";
     public Thickness AppBorderThickness { get; set; } = Thickness.Thin;
 }
