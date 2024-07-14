200af1056ca1574d931ee419bbe92b6105b11c63
zoltanvi <viczezoltan@gmail.com>
2024-07-14 11:47:23 +0200

    Make recycle bin group collapsable

--- Modules.RecycleBin.Views/Controls/RecycleBinGroupItemViewModel.cs
+++ Modules.RecycleBin.Views/Controls/RecycleBinGroupItemViewModel.cs
@@ -1,11 +1,20 @@
-﻿using Modules.Common.ViewModel;
+﻿using Modules.Common.DataBinding;
+using Modules.Common.ViewModel;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Modules.RecycleBin.Views.Controls;
 
 public class RecycleBinGroupItemViewModel : BaseViewModel
 {
+    public RecycleBinGroupItemViewModel()
+    {
+        ToggleGroupIsOpen = new RelayCommand(() => IsOpen ^= true);
+    }
+
     public int CategoryId { get; set; }
     public string CategoryName { get; set; }
     public ObservableCollection<RecycleBinTaskItemViewModel> Items { get; set; }
+    public ICommand ToggleGroupIsOpen { get; }
+    public bool IsOpen { get; set; } = true;
 }
