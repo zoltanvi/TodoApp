260a45b3fe58e49cb186ce05394c95a9c9d97b71
zoltanvi <viczezoltan@gmail.com>
2024-07-21 12:44:50 +0200

    Allow reopening current category

--- Modules.Categories.Views/Pages/CategoryListPageViewModel.cs
+++ Modules.Categories.Views/Pages/CategoryListPageViewModel.cs
@@ -150,7 +150,7 @@ public class CategoryListPageViewModel : BaseViewModel
         _categoriesRepository.DeleteCategory(category.Map());
 
         _mediator.Send(new ShowMessageInfoCommand { Message = $"Deleted category: {category.Name}" });
-        
+
         CategoryDeletedEvent.Invoke(new CategoryDeletedEvent { CategoryId = category.Id });
 
         // Only if the current category was the deleted one, select a new category
@@ -170,14 +170,14 @@ public class CategoryListPageViewModel : BaseViewModel
 
             AppSettings.Instance.SessionSettings.ActiveCategoryId = ActiveCategoryId;
 
-            _mediator.Publish(new ActiveCategoryChangedEvent
-            {
-                CategoryId = category.Id,
-                CategoryName = category.Name
-            });
-
             //IoC.NoteListService.ActiveNote = null;
         }
+
+        _mediator.Publish(new ActiveCategoryChangedEvent
+        {
+            CategoryId = category.Id,
+            CategoryName = category.Name
+        });
     }
 
     private void OpenSettingsPage()
