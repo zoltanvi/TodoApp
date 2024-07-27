532b3b9260f6f96ce77625b272859b5d06283d87
zoltanvi <viczezoltan@gmail.com>
2024-07-27 08:47:49 +0200

    Load back saved scaling value to UIScaler

--- Modules.Settings.Services/CqrsHandling/EventHandlers/ApplicationOpeningEventHandler.cs
+++ Modules.Settings.Services/CqrsHandling/EventHandlers/ApplicationOpeningEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Modules.Common.Cqrs.Events;
+using Modules.Common.Services;
 using Modules.Settings.Contracts.ViewModels;
 
 namespace Modules.Settings.Services.CqrsHandling.EventHandlers;
@@ -7,16 +8,24 @@ namespace Modules.Settings.Services.CqrsHandling.EventHandlers;
 public class ApplicationOpeningEventHandler : INotificationHandler<ApplicationOpeningEvent>
 {
     private readonly IAppSettingsService _appSettingsService;
+    private readonly IUIScaler _uiScaler;
 
-    public ApplicationOpeningEventHandler(IAppSettingsService appSettingsService)
+    public ApplicationOpeningEventHandler(IAppSettingsService appSettingsService, IUIScaler uiScaler)
     {
         ArgumentNullException.ThrowIfNull(appSettingsService);
+        ArgumentNullException.ThrowIfNull(uiScaler);
+
         _appSettingsService = appSettingsService;
+        _uiScaler = uiScaler;
     }
 
     public Task Handle(ApplicationOpeningEvent notification, CancellationToken cancellationToken)
     {
         _appSettingsService.UpdateAppSettingsFromDatabase(AppSettings.Instance);
+
+        // Set the saved scaling to restore the UI
+        _uiScaler.SetScaling(AppSettings.Instance.WindowSettings.Scaling);
+        
         return Task.CompletedTask;
     }
 }
