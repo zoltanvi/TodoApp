bfe5068343e392b7b094c391440c4b69483ab9c4
zoltanvi <viczezoltan@gmail.com>
2024-08-06 20:14:03 +0200

    Change default modification date format

--- Modules.Settings.Contracts/ViewModels/DateTimeSettings.cs
+++ Modules.Settings.Contracts/ViewModels/DateTimeSettings.cs
@@ -5,6 +5,6 @@ public class DateTimeSettings : SettingsBase
     public bool TitleBarDateVisible { get; set; }
     public string TitleBarDateFormat { get; set; } = "HH:mm:ss";
     public string ReminderDateFormat { get; set; } = "yyyy MMMM d. dddd, HH:mm:ss";
-    public string TaskCreationDateFormat { get; set; } = "yyyy-MM-dd HH:mm";
+    public string TaskCreationDateFormat { get; set; } = "yyyy-MM-dd HH:mm:ss";
 }
 
