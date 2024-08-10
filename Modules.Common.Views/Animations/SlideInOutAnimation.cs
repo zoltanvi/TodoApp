bfdb5770cf587bd0549300d992eba5522460b107
zoltanvi <viczezoltan@gmail.com>
2024-08-10 14:33:56 +0200

    Handle popup messages without a queue

---
+++ Modules.Common.Views/Animations/SlideInOutAnimation.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace Modules.Common.Views.Animations;
+
+public static class SlideInOutAnimation
+{
+    public static readonly DependencyProperty IsAnimatingProperty =
+        DependencyProperty.RegisterAttached("IsAnimating", typeof(bool), typeof(SlideInOutAnimation),
+            new PropertyMetadata(false, OnIsAnimatingChanged));
+
+    public static readonly DependencyProperty DurationProperty =
+        DependencyProperty.RegisterAttached("Duration", typeof(TimeSpan), typeof(SlideInOutAnimation),
+            new PropertyMetadata(TimeSpan.FromSeconds(5)));
+
+    private static readonly DependencyProperty IsVisibleProperty =
+        DependencyProperty.RegisterAttached("IsVisible", typeof(bool), typeof(SlideInOutAnimation), new PropertyMetadata(false));
+
+    private static readonly DependencyProperty AnimationTimerProperty =
+        DependencyProperty.RegisterAttached("AnimationTimer", typeof(DispatcherTimer), typeof(SlideInOutAnimation));
+
+    public static bool GetIsAnimating(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(IsAnimatingProperty);
+    }
+
+    public static void SetIsAnimating(DependencyObject obj, bool value)
+    {
+        obj.SetValue(IsAnimatingProperty, value);
+    }
+
+    public static TimeSpan GetDuration(DependencyObject obj)
+    {
+        return (TimeSpan)obj.GetValue(DurationProperty);
+    }
+
+    public static void SetDuration(DependencyObject obj, TimeSpan value)
+    {
+        obj.SetValue(DurationProperty, value);
+    }
+
+    private static bool GetIsVisible(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(IsVisibleProperty);
+    }
+
+    private static void SetIsVisible(DependencyObject obj, bool value)
+    {
+        obj.SetValue(IsVisibleProperty, value);
+    }
+
+    private static DispatcherTimer GetAnimationTimer(DependencyObject obj)
+    {
+        return (DispatcherTimer)obj.GetValue(AnimationTimerProperty);
+    }
+
+    private static void SetAnimationTimer(DependencyObject obj, DispatcherTimer value)
+    {
+        obj.SetValue(AnimationTimerProperty, value);
+    }
+
+    private static void OnIsAnimatingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            if ((bool)e.NewValue)
+            {
+                // If IsAnimating is true, start or reset the animation
+                StartOrResetAnimation(element);
+            }
+        }
+    }
+
+    private static void StartOrResetAnimation(FrameworkElement element)
+    {
+        TimeSpan duration = GetDuration(element);
+
+        // If the element is already visible, reset the timer
+        if (GetIsVisible(element))
+        {
+            var timer = GetAnimationTimer(element);
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Interval = duration;
+                timer.Start();
+            }
+        }
+        else
+        {
+            SlideIn(element);
+
+            // Set the element as visible and start the timer to slide out after the duration
+            SetIsVisible(element, true);
+            var timer = new DispatcherTimer { Interval = duration };
+            timer.Tick += (s, e) =>
+            {
+                SlideOut(element);
+                timer.Stop();
+                SetIsVisible(element, false);
+            };
+            timer.Start();
+
+            // Store the timer in the attached property for future reference
+            SetAnimationTimer(element, timer);
+        }
+    }
+
+    private static void SlideIn(FrameworkElement element)
+    {
+        var height = element.ActualHeight;
+        var slideInAnimation = new DoubleAnimation
+        {
+            From = -height,
+            To = 0,
+            Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+            EasingFunction = new BackEase { EasingMode = EasingMode.EaseIn }
+        };
+
+        element.RenderTransform = new TranslateTransform();
+        element.RenderTransform.BeginAnimation(TranslateTransform.YProperty, slideInAnimation);
+    }
+
+    private static void SlideOut(FrameworkElement element)
+    {
+        var height = element.ActualHeight;
+        var slideOutAnimation = new DoubleAnimation
+        {
+            From = 0,
+            To = -height,
+            Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+            EasingFunction = new BackEase { EasingMode = EasingMode.EaseOut }
+        };
+
+        element.RenderTransform = new TranslateTransform();
+        element.RenderTransform.BeginAnimation(TranslateTransform.YProperty, slideOutAnimation);
+    }
+}
--- Modules.PopupMessage.Views/PopupMessageManager.cs
+++ Modules.PopupMessage.Views/PopupMessageManager.cs
@@ -1,5 +1,4 @@
 ﻿using Modules.Common.ViewModel;
-using Modules.Common.Views.Services;
 using Modules.PopupMessage.Views.Models;
 using Modules.Settings.Contracts.ViewModels;
 using PropertyChanged;
@@ -9,18 +8,10 @@ namespace Modules.PopupMessage.Views;
 [AddINotifyPropertyChangedInterface]
 public class PopupMessageManager : BaseViewModel
 {
-    private readonly Guid _timer;
-    private bool _visible;
-    private const int AnimationMilliseconds = 1500;
-    private readonly Queue<PopupMessage> _messageQueue;
-    private int _currentMessageDuration;
-
     public static PopupMessageManager Instance { get; } = new();
 
     private PopupMessageManager()
     {
-        _timer = TimerService.Instance.CreateTimer(OnMessageDurationEnded);
-        _messageQueue = new Queue<PopupMessage>();
         AppSettings.Instance.ThemeSettings.SettingsChanged += OnThemeSettingsChanged;
     }
 
@@ -34,66 +25,22 @@ public class PopupMessageManager : BaseViewModel
 
     public void ShowMessage(string message, MessageType messageType, TimeSpan duration)
     {
-        _messageQueue.Enqueue(new PopupMessage
-        {
-            Message = message,
-            MessageType = messageType,
-            Duration = duration
-        });
+        MessageType = messageType;
+        Message = message;
 
-        ProcessNextMessage();
+        // Set the duration first for the animation. Adding one second for the slide in and out animations
+        MessageDuration = duration + TimeSpan.FromSeconds(1);
+
+        // Changing it to true triggers the animation, changing back to false does not.
+        Visible = true;
+        Visible = false;
+
+        // Make sure to trigger ui update
+        OnPropertyChanged(nameof(MessageType));
     }
 
     public MessageType MessageType { get; set; } = MessageType.Invalid;
-
     public string Message { get; set; } = string.Empty;
-
-    public bool Visible
-    {
-        get => _visible;
-        set
-        {
-            var wasVisible = _visible;
-
-            _visible = value;
-
-            if (_visible)
-            {
-                // Add the animation time to the display time if the message line is not visible yet
-                var duration = wasVisible
-                    ? _currentMessageDuration
-                    : _currentMessageDuration + AnimationMilliseconds;
-
-                TimerService.Instance.StopTimer(_timer);
-                TimerService.Instance.ModifyTimerInterval(_timer, duration);
-                TimerService.Instance.RestartTimer(_timer);
-            }
-        }
-    }
-
-    private void ProcessNextMessage()
-    {
-        // Uncomment this prevent interrupting the currently displayed message
-        if (/*!Visible && */_messageQueue.Count != 0)
-        {
-            var messageItem = _messageQueue.Dequeue();
-
-            MessageType = messageItem.MessageType;
-            Message = messageItem.Message;
-            _currentMessageDuration = (int)messageItem.Duration.TotalMilliseconds;
-
-            Visible = true;
-
-            // Make sure to trigger ui update
-            OnPropertyChanged(nameof(MessageType));
-        }
-    }
-
-    private void OnMessageDurationEnded(object? sender, EventArgs e)
-    {
-        TimerService.Instance.StopTimer(_timer);
-        Visible = false;
-
-        ProcessNextMessage();
-    }
+    public bool Visible { get; set; } = false;
+    public TimeSpan MessageDuration { get; set; } = TimeSpan.FromSeconds(4);
 }
