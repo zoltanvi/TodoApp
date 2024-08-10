89b677e1115da2af21d88e691d010544832db23a
zoltanvi <viczezoltan@gmail.com>
2024-08-10 17:35:33 +0200

    Fix stucked edit mode for tasks when category changed during editing a task

---
+++ Modules.Tasks.Views/CqrsHandling/TaskViewActiveCategoryChangedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Modules.Categories.Contracts.Cqrs.Events;
+using Modules.Tasks.Views.Services;
+
+namespace Modules.Tasks.Views.CqrsHandling;
+
+public class TaskViewActiveCategoryChangedEventHandler : INotificationHandler<ActiveCategoryChangedEvent>
+{
+    private readonly OneEditorOpenService _oneEditorOpenService;
+
+    public TaskViewActiveCategoryChangedEventHandler(OneEditorOpenService oneEditorOpenService)
+    {
+        ArgumentNullException.ThrowIfNull(oneEditorOpenService);
+        _oneEditorOpenService = oneEditorOpenService;
+    }
+
+    public Task Handle(ActiveCategoryChangedEvent notification, CancellationToken cancellationToken)
+    {
+        _oneEditorOpenService.EditModeWithoutTask();
+        return Task.CompletedTask;
+    }
+}
