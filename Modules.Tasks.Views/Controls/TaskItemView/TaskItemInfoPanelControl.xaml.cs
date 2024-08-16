2ba355fe365dc4c0deab15a3ca079d0061a144ae
zoltanvi <viczezoltan@gmail.com>
2024-08-16 09:42:29 +0200

    Make a control for task item info panel

---
+++ Modules.Tasks.Views/Controls/TaskItemView/TaskItemInfoPanelControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Modules.Tasks.Views.Controls.TaskItemView;
+
+public partial class TaskItemInfoPanelControl : UserControl
+{
+    public TaskItemInfoPanelControl()
+    {
+        InitializeComponent();
+    }
+}
