2218a78dd56a685f5262483ec517d3ba493ebc40
zoltanvi <viczezoltan@gmail.com>
2024-08-18 13:35:38 +0200

    Fix task popup position is wrong when scaled

---
+++ Modules.Tasks.Views/AttachedProperties/FirstTaskItemMarginScaler.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+
+namespace Modules.Tasks.Views.AttachedProperties;
+
+public static class FirstTaskItemMarginScaler
+{
+    public static readonly DependencyProperty TopMarginProperty = DependencyProperty.RegisterAttached("TopMargin", typeof(double), typeof(FirstTaskItemMarginScaler), new PropertyMetadata(0.0));
+    public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.RegisterAttached("ScaleValue", typeof(double), typeof(FirstTaskItemMarginScaler), new PropertyMetadata(0.0, OnScaleValueChanged));
+    public static readonly DependencyProperty IsFirstItemProperty = DependencyProperty.RegisterAttached("IsFirstItem", typeof(bool), typeof(FirstTaskItemMarginScaler), new PropertyMetadata(false, OnIsFirstItemChanged));
+
+
+    public static void SetTopMargin(DependencyObject element, double value)
+    {
+        element.SetValue(TopMarginProperty, value);
+    }
+
+    public static double GetTopMargin(DependencyObject element)
+    {
+        return (double)element.GetValue(TopMarginProperty);
+    }
+
+
+    public static void SetScaleValue(DependencyObject element, double value)
+    {
+        element.SetValue(ScaleValueProperty, value);
+    }
+
+    public static double GetScaleValue(DependencyObject element)
+    {
+        return (double)element.GetValue(ScaleValueProperty);
+    }
+
+
+    public static void SetIsFirstItem(DependencyObject element, bool value)
+    {
+        element.SetValue(IsFirstItemProperty, value);
+    }
+
+    public static bool GetIsFirstItem(DependencyObject element)
+    {
+        return (bool)element.GetValue(IsFirstItemProperty);
+    }
+
+    private static void OnScaleValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            UpdateMargin(element);
+        }
+    }
+
+
+    private static void OnIsFirstItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            UpdateMargin(element);
+        }
+    }
+
+    private static void UpdateMargin(FrameworkElement element)
+    {
+        var isFirstItem = GetIsFirstItem(element);
+        var topMargin = GetTopMargin(element);
+        var scaleValue = GetScaleValue(element);
+
+        var resultMargin = isFirstItem ? topMargin * scaleValue : 0;
+
+        element.Margin = new Thickness(0, resultMargin, 0, 0);
+    }
+}
---
+++ Modules.Tasks.Views/AttachedProperties/TaskPopupMarginScaler.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+
+namespace Modules.Tasks.Views.AttachedProperties;
+
+public static class TaskPopupMarginScaler
+{
+    public static readonly DependencyProperty VerticalOffsetProperty = DependencyProperty.RegisterAttached("VerticalOffset", typeof(double), typeof(TaskPopupMarginScaler), new PropertyMetadata(0.0, OnOffsetsChanged));
+    public static readonly DependencyProperty HorizontalOffsetProperty = DependencyProperty.RegisterAttached("HorizontalOffset", typeof(double), typeof(TaskPopupMarginScaler), new PropertyMetadata(0.0, OnOffsetsChanged));
+    public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.RegisterAttached("ScaleValue", typeof(double), typeof(TaskPopupMarginScaler), new PropertyMetadata(1.0, OnScaleValueChanged));
+
+    public static void SetVerticalOffset(UIElement element, double value)
+    {
+        element.SetValue(VerticalOffsetProperty, value);
+    }
+
+    public static double GetVerticalOffset(UIElement element)
+    {
+        return (double)element.GetValue(VerticalOffsetProperty);
+    }
+
+    public static void SetHorizontalOffset(UIElement element, double value)
+    {
+        element.SetValue(HorizontalOffsetProperty, value);
+    }
+
+    public static double GetHorizontalOffset(UIElement element)
+    {
+        return (double)element.GetValue(HorizontalOffsetProperty);
+    }
+
+    public static void SetScaleValue(UIElement element, double value)
+    {
+        element.SetValue(ScaleValueProperty, value);
+    }
+
+    public static double GetScaleValue(UIElement element)
+    {
+        return (double)element.GetValue(ScaleValueProperty);
+    }
+
+    private static void OnOffsetsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            UpdateMargin(element);
+        }
+    }
+
+    private static void OnScaleValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            UpdateMargin(element);
+        }
+    }
+
+    // Update the Margin based on the offsets and scale value
+    private static void UpdateMargin(FrameworkElement element)
+    {
+        double verticalOffset = GetVerticalOffset(element);
+        double horizontalOffset = GetHorizontalOffset(element);
+        double scaleValue = GetScaleValue(element);
+
+        element.Margin = new Thickness(
+            -horizontalOffset * scaleValue,
+            -verticalOffset * scaleValue,
+            horizontalOffset * scaleValue,
+            verticalOffset * scaleValue);
+    }
+}
