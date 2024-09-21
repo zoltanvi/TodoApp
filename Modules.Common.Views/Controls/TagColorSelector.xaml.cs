03e666c9f7786cfccbfbae12a8c0b77ab649f43e
zoltanvi <viczezoltan@gmail.com>
2024-09-21 10:18:04 +0200

    Replace combobox with tag color selector

--- Modules.Common.Views/Controls/TagColorSelector.xaml.cs
+++ Modules.Common.Views/Controls/TagColorSelector.xaml.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modules.Common.DataModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace Modules.Common.Views.Controls;
-/// <summary>
-/// Interaction logic for TagColorSelector.xaml
-/// </summary>
+
 public partial class TagColorSelector : UserControl
 {
+    public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(
+        nameof(SelectedColor), typeof(TagColor), typeof(TagColorSelector), new PropertyMetadata(default(TagColor)));
+
+    public TagColor SelectedColor
+    {
+        get => (TagColor)GetValue(SelectedColorProperty);
+        set => SetValue(SelectedColorProperty, value);
+    }
+
     public TagColorSelector()
     {
         InitializeComponent();
+    }
+
+    private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+    {
+        if (sender is ToggleButton toggleButton &&
+            toggleButton.Tag is TagColor colorToSelect)
+        {
+            SelectedColor = colorToSelect;
+        }
     }
 }
